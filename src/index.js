4619f5e6a0b5777c5d982e8c2df2815c9a15e6f0
DigitalKnight0 <asadredset@gmail.com>
2022-07-06 20:11:40 +0500

    Fix linting problems

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-import { getWeather } from "./weather";
-import populateDisplay from './View'
+import getWeather from './weather';
+import populateDisplay from './View';
 
-const unitBtn = document.querySelector('.unit-btn')
+const unitBtn = document.querySelector('.unit-btn');
 
 const fetchData = async () => {
-    try{
-        const cityName = document.querySelector('.search-bar').value
-        const unitSystem = unitBtn.checked ? 'metric' : 'imperial'
-        const data = await getWeather(cityName, unitSystem)
-        if(data instanceof Error) throw data
-        populateDisplay(data)
-    } catch(error){
-        alert(error)
-    }
-}
+  try {
+    const cityName = document.querySelector('.search-bar').value;
+    const unitSystem = unitBtn.checked ? 'metric' : 'imperial';
+    const data = await getWeather(cityName, unitSystem);
+    if (data instanceof Error) throw data;
+    populateDisplay(data);
+  } catch (error) {
+    alert(error);
+  }
+};
 
-unitBtn.addEventListener('input', (e) => {
-    const cityName = document.querySelector('.name').textContent
-    document.querySelector('.search-bar').value = cityName
-    fetchData()
-    document.querySelector('.search-bar').value = ''
-})
+unitBtn.addEventListener('input', () => {
+  const cityName = document.querySelector('.name').textContent;
+  document.querySelector('.search-bar').value = cityName;
+  fetchData();
+  document.querySelector('.search-bar').value = '';
+});
 
 document.querySelector('.search').addEventListener('click', fetchData);
 window.addEventListener('keydown', (e) => {
-    if(document.activeElement === document.querySelector('.search-bar') && e.key === 'Enter') fetchData()
+  if (document.activeElement === document.querySelector('.search-bar') && e.key === 'Enter') fetchData();
 });
 
 (async () => {
-    const data = await getWeather('london', 'imperial')
-    populateDisplay(data)
-})()
+  const data = await getWeather('london', 'imperial');
+  populateDisplay(data);
+})();
