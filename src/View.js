304b37c510e8c1e88bbd7b685aa4ff9d25ba6538
DigitalKnight0 <asadredset@gmail.com>
2022-07-06 20:06:23 +0500

    Add styling to page

---
+++ src/View.js
@@ -0,0 +1,20 @@
+const cityName = document.querySelector('.name')
+const weatherIcon = document.querySelector('.main-icon')
+const type = document.querySelector('.type')
+const temp = document.querySelector('.temp')
+const feelsLike = document.querySelector('.feels-like')
+const wind = document.querySelector('.wind')
+const humidity = document.querySelector('.humidity')
+
+const populateDisplay = (data) => {
+    cityName.textContent = data.name
+    weatherIcon.src = data.iconUrl
+    type.textContent = data.weatherType
+    temp.textContent = `Temprature: ${data.temp}`
+    feelsLike.textContent = `Feels Like: ${data.tempFeelsLike}`
+    wind.textContent = `Wind Speed: ${data.wind}`
+    humidity.textContent = `Humidity: ${data.humidity}`
+}
+
+export default populateDisplay
+
---
+++ src/weather.js
@@ -0,0 +1,34 @@
+const filterData = (response) => {
+    return {
+        name: response.name,
+        iconUrl: `http://openweathermap.org/img/wn/${response.weather['0'].icon}@2x.png`,
+        weatherType: response.weather['0'].description,
+        temp: response.main.temp,
+        tempFeelsLike: response.main.feels_like,
+        wind: response.wind.speed,
+        humidity: response.main.humidity
+    }
+}
+
+const formatUnits = (data, system) => {
+    const [tempUnit, windUnit] = system === 'imperial' ? ['\u00B0 F', 'mph'] : ['\u00B0 C', 'm/s']
+    data.temp = data.temp + ' ' + tempUnit
+    data.tempFeelsLike = data.tempFeelsLike + ' ' + tempUnit
+    data.wind = data.wind + ' ' + windUnit
+    data.humidity = data.humidity + ' %'
+}
+
+const getWeather = async (location, unitSystem) => {
+    try{
+        const data = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unitSystem}&appid=6f404c6c2feb1ace89f818a103284d50`)).json()
+        if(data.message) throw new Error(data.message)
+        const filteredData = filterData(data)
+        formatUnits(filteredData, unitSystem)
+        return filteredData
+    }
+    catch(error){
+        return error
+    }
+}
+
+export {getWeather}
